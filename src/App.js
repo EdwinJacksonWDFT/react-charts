2dd84c20cad417700e26c6f8bf7dbe81552b8795
Edwin Jackson <ejackson@konradgroup.com>
2019-03-06 15:34:04 -0500

    scatter plot

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import BarChart from './components/BarChart';
 import PieChart from './components/PieChart';
+import ScatterPlot from './components/ScatterPlot';
 import './App.css';
 
 import basicBarData from './data/basic-bar';
 import basicPieData from './data/basic-pie';
+import basicScatter from './data/basic-scatter';
 
 class App extends Component {
   render() {
@@ -12,6 +14,7 @@ class App extends Component {
       <div className="App">
         <BarChart data={basicBarData} height={100} width={50} />
         <PieChart title="Commuting to work in Toronto" data={basicPieData} height={20} width={20} />
+        <ScatterPlot title="Scatter plot" data={basicScatter} height={100} width={100} />
       </div>
     );
   }
